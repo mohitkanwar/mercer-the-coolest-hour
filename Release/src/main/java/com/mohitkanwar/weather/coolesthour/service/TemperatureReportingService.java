65eae9155a6d1fb4cbfeb0a3e9a6fa7d4814d9b3
Mohit Kanwar <m.m.kanwar@gmail.com>
2018-12-10 11:13:20 +0530

    removed src from release, since code can be shared via github

--- Release/src/main/java/com/mohitkanwar/weather/coolesthour/service/TemperatureReportingService.java
+++
@@ -1,9 +0,0 @@
-package com.mohitkanwar.weather.coolesthour.service;
-
-import com.mohitkanwar.weather.coolesthour.model.TemperatureAtTime;
-
-import java.util.List;
-
-public interface TemperatureReportingService {
-    void printTemperatures(List<TemperatureAtTime> temperatures);
-}
--- Release/src/main/java/com/mohitkanwar/weather/coolesthour/service/ZipCodeDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.mohitkanwar.weather.coolesthour.service;
-
-import com.mohitkanwar.weather.coolesthour.model.Location;
-
-public interface ZipCodeDetailsService {
-    Location getLocationFromZipCode(String zipcode);
-}
